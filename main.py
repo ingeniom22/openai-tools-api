19aeacfa92a7815d5b02dae061240ebe139f7b63
Mohamad Arsya Kaukabi <345779@corp.bri.co.id>
2025-01-12 10:13:06 +0700

    update prompt

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 import os
-from langchain_neo4j import Neo4jGraph, GraphCypherQAChain
-from langchain_openai import ChatOpenAI
+
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_neo4j import GraphCypherQAChain, Neo4jGraph
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel
 
 # Load environment variables
 load_dotenv()
@@ -20,20 +22,43 @@ neo4j_graph_store = Neo4jGraph(
     url=NEO4J_URI,
     username=NEO4J_USERNAME,
     password=NEO4J_PASSWORD,
+    enhanced_schema=True,
 )
 
 
 # Function to retrieve data from the knowledge graph
 def retrieve_kg(question: str):
     """Retrieve data from knowledge graph to answer user question"""
+    CYPHER_GENERATION_TEMPLATE = """Task:Generate Cypher statement to query a graph database.
+Instructions:
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+Schema:
+{schema}
+Note: Do not include any explanations or apologies in your responses.
+Do not respond to any questions that might ask anything else than for you to construct a Cypher statement.
+Do not include any text except the generated Cypher statement.
+Examples: Here are a few examples of generated Cypher statements for particular questions:
+# What is the definition of contract?
+MATCH (n) WHERE n.n4sch__name = "Contract" RETURN n
+
+The question is:
+{question}"""
+
+    CYPHER_GENERATION_PROMPT = PromptTemplate(
+        input_variables=["schema", "question"], template=CYPHER_GENERATION_TEMPLATE
+    )
     try:
         chain = GraphCypherQAChain.from_llm(
             llm,
             graph=neo4j_graph_store,
             verbose=True,
             allow_dangerous_requests=True,
+            validate_cypher=True,
+            cypher_prompt=CYPHER_GENERATION_PROMPT,
         )
-        result = chain.run(question)
+
+        result = chain.invoke({"query": question})
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
